c0e0025b2bcb71455bfd4b56858cbefcdbb25a82
Robin Deits <robin.deits@gmail.com>
2017-02-20 22:55:23 -0500

    use nbinclude to test notebooks

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,13 @@
 using Base.Test
 using RegionTrees
 using StaticArrays
+using NBInclude
 
 include("hyperrectangle.jl")
 include("cell.jl")
 include("twosarray.jl")
 include("asdfs.jl")
+
+const notebooks_dir = joinpath(dirname(@__FILE__), "..", "examples")
+nbinclude(joinpath(notebooks_dir, "demo.ipynb"))
+nbinclude(joinpath(notebooks_dir, "adaptive_distances.ipynb"))
