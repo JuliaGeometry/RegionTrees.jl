1468339728d0020042565ac7f5f8ec9e90935a25
Robin Deits <robin.deits@gmail.com>
2018-11-08 08:12:58 -0500

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,26 @@
 using Test
 using RegionTrees
 using StaticArrays
-using IJulia
 using LinearAlgebra
+using NBInclude
 
 include("hyperrectangle.jl")
 include("cell.jl")
 include("twosarray.jl")
 include("asdfs.jl")
 
-const notebooks_dir = joinpath(dirname(@__FILE__), "..", "examples")
+const notebooks_dir = joinpath(@__DIR__, "..", "examples")
 
+# """
+# Run a jupyter notebook (.ipynb) file using NBInclude. Spawns a
+# new Julia session so that notebooks that use a different project
+# directory still work.
+# """
 function execute_notebook(path)
-    mktempdir() do dir
-        run(`$(IJulia.jupyter) nbconvert --execute $path --output-dir=$dir`)
+    # julia = joinpath(Sys.BINDIR, "julia")
+    # run(`$julia --project=$(dirname(path)) -e "using NBInclude; @nbinclude(\"$path\")"`)
+    cd(dirname(path)) do
+        @nbinclude(path)
     end
 end
 
