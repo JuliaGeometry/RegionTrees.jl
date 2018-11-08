477d78c0e4749800a8c8c2c52e6b3c82f092521b
Robin Deits <robin.deits@gmail.com>
2018-11-08 16:58:55 -0500

    add Pkg to demo projects

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,15 +11,9 @@ include("asdfs.jl")
 
 const notebooks_dir = joinpath(@__DIR__, "..", "examples")
 
-"""
-Run a jupyter notebook (.ipynb) file using NBInclude. Spawns a
-new Julia session so that notebooks that use a different project
-directory work reliably.
-"""
 function execute_notebook(path)
-    julia = joinpath(Sys.BINDIR, "julia")
     cd(dirname(path)) do
-        run(`$julia --project=$(dirname(path)) -e "using NBInclude; @nbinclude(\"$path\")"`)
+        @nbinclude(path)
     end
 end
 
