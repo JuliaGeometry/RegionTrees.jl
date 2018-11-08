2fac3a3d5c2f66e6eacd94443aa9cd231030ee49
Robin Deits <robin.deits@gmail.com>
2018-11-08 16:47:58 -0500

    test notebooks in separate session with cd

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,16 +11,15 @@ include("asdfs.jl")
 
 const notebooks_dir = joinpath(@__DIR__, "..", "examples")
 
-# """
-# Run a jupyter notebook (.ipynb) file using NBInclude. Spawns a
-# new Julia session so that notebooks that use a different project
-# directory still work.
-# """
+"""
+Run a jupyter notebook (.ipynb) file using NBInclude. Spawns a
+new Julia session so that notebooks that use a different project
+directory work reliably.
+"""
 function execute_notebook(path)
-    # julia = joinpath(Sys.BINDIR, "julia")
-    # run(`$julia --project=$(dirname(path)) -e "using NBInclude; @nbinclude(\"$path\")"`)
+    julia = joinpath(Sys.BINDIR, "julia")
     cd(dirname(path)) do
-        @nbinclude(path)
+        run(`$julia --project=$(dirname(path)) -e "using NBInclude; @nbinclude(\"$path\")"`)
     end
 end
 
