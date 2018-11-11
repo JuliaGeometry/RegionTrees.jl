45db0838a98bc36ae1b7bebba4e3f474b44732cc
Robin Deits <robin.deits@gmail.com>
2018-11-11 11:30:19 -0500

    don't worry about notebook tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,21 +2,8 @@ using Test
 using RegionTrees
 using StaticArrays
 using LinearAlgebra
-using NBInclude
 
 include("hyperrectangle.jl")
 include("cell.jl")
 include("twosarray.jl")
 include("asdfs.jl")
-
-const notebooks_dir = joinpath(@__DIR__, "..", "examples")
-
-function execute_notebook(path)
-    julia = joinpath(Sys.BINDIR, "julia")
-    cd(dirname(path)) do
-        run(`$julia --project=$(dirname(path)) -e "using NBInclude; @nbinclude(\"$path\")"`)
-    end
-end
-
-execute_notebook(joinpath(notebooks_dir, "demo", "demo.ipynb"))
-execute_notebook(joinpath(notebooks_dir, "adaptive_distance_fields", "adaptive_distances.ipynb"))
