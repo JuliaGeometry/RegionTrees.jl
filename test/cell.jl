7581abe2d2cd28ce3735816f5c1ab82921f17bc0
Tamás Cserteg <csertegt@protonmail.com>
2026-07-08 17:30:15 +0200

    define refinery in tests only once

--- test/cell.jl
+++ test/cell.jl
@@ -67,19 +67,6 @@ end
 end
 
 @testset "allcells, alleaves, allparents functions" begin
-    struct MyRefinery <: RegionTrees.AbstractRefinery
-        tolerance::Float64
-    end
-
-    function RegionTrees.needs_refinement(r::MyRefinery, cell)
-        maximum(cell.boundary.widths) > r.tolerance
-    end
-
-    function RegionTrees.refine_data(r::MyRefinery, cell::Cell, indices)
-        boundary = child_boundary(cell, indices)
-        "child with widths: $(boundary.widths)"
-    end
-
     r = MyRefinery(0.5)
     root = Cell(SVector(0., 0), SVector(1., 1), "root")
     adaptivesampling!(root, r)
